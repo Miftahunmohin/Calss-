c73a616afd318b5dfe87714a951241ae39b3cc4b
nazmulkp <nazmulkp053@gmail.com>
2014-09-27 16:00:09 +0600

    Circle apps

---
+++ Circle apps/Circle apps/Circle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle_apps
+{
+    class Circle
+    {
+       public double radias;
+        public double GetArea()
+        {
+            return 2 * radias;
+        }
+
+    public double GetPerimeter()
+        {
+            return 2 * Math.PI * radias;
+        }
+
+    public double GetDiameter()
+        {
+            return Math.PI * radias * radias;
+        }
+
+   
+    }
+}
---
+++ Circle apps/Circle apps/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle_apps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle aCircle = new Circle();
+            aCircle.radias = 269;
+           Console.WriteLine("area is :"+ aCircle.GetArea());
+           Console.WriteLine("Perimeter is :" + aCircle.GetPerimeter());
+           Console.WriteLine("Diameter is :" + aCircle.GetDiameter());
+             Console.ReadLine();
+        }
+    }
+}
